06b008f53e7a0acd9637017eb7adb1f535021af4
Александр <mas2005@inbox.ru>
2022-05-25 12:11:26 +0300

    10

---
+++ 10/app.jsx
@@ -0,0 +1,39 @@
+import Header from "./header.jsx";
+import Main from "./main.jsx";
+
+export default () => {
+  return (
+    <React.Fragment>
+      <Header>
+        <h1>Товары для котиков</h1>
+      </Header>
+      <Main>
+        <ul className="products">
+          <li>
+            <article className="product product--sale">
+              <mark>Сейл</mark>
+              <h3><a href="#">Пищалка</a></h3>
+              <p>500 ₽</p>
+            </article>
+          </li>
+          <li>
+            <article className="product product--new">
+              <mark>Нью</mark>
+              <h3><a href="#">Дразнилка</a></h3>
+              <p>5 ₽</p>
+            </article>
+          </li>
+          <li>
+            <article className="product">
+              <h3><a href="#">Лежанка</a></h3>
+              <p>2400 ₽</p>
+            </article>
+          </li>
+        </ul>
+      </Main>
+    </React.Fragment>
+  );
+};
+Двигаемся дальше. Давайте разберёмся со списком товаров, превратим его из статичной разметки в живой список.
+
+Этот подход вам уже знаком, мы отделим данные от шаблона в массив и отрисуем по массиву. Начнём с данных.
