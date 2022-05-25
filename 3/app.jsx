2f91378aa4487315eb2c579d4892ed3895e90531
Александр <mas2005@inbox.ru>
2022-05-25 12:09:59 +0300

    3

---
+++ 3/app.jsx
@@ -0,0 +1,37 @@
+import Header from "./header.jsx";
+import Main from "./main.jsx";
+
+export default () => {
+  return (
+    <React.Fragment>
+      <Header>
+        <h1>Товары для котиков</h1>
+      </Header>
+      <Main>
+        <ul className="products">
+          <li>
+            <article className="product product--sale">
+              <mark>Сейл</mark>
+              <h3><a href="#">Пищалка</a></h3>
+              <p>500 ₽</p>
+            </article>
+          </li>
+          <li>
+            <article className="product product--new">
+              <mark>Нью</mark>
+              <h3><a href="#">Дразнилка</a></h3>
+              <p>5 ₽</p>
+            </article>
+          </li>
+          <li>
+            <article className="product">
+              <h3><a href="#">Лежанка</a></h3>
+              <p>2400 ₽</p>
+            </article>
+          </li>
+        </ul>
+      </Main>
+    </React.Fragment>
+  );
+};
+Теперь вместо тегов используем наши компоненты. Обратите внимание на результат, он не изменился, а значит всё работает корректно.
