d117bf2191165abd586744ab4537f85eff4110eb
Александр <mas2005@inbox.ru>
2022-05-25 12:16:05 +0300

    32

---
+++ 32/app.jsx
@@ -0,0 +1,18 @@
+import Header from "./header.jsx";
+import Main from "./main.jsx";
+import ProductList from "./product-list.jsx";
+import {products} from "../data.js";
+
+export default () => {
+  return (
+    <React.Fragment>
+      <Header>
+        <h1>Товары для котиков</h1>
+      </Header>
+      <Main>
+        <ProductList products={products} />
+      </Main>
+    </React.Fragment>
+  );
+};
+Возвращаем нужные компоненты.
