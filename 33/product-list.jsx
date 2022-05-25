694e21a5447db0cb0116ba1dc18bcee6bd980011
Александр <mas2005@inbox.ru>
2022-05-25 12:16:18 +0300

    33

---
+++ 33/product-list.jsx
@@ -0,0 +1,29 @@
+import Product from "./product.jsx";
+import ProductNew from "./product-new.jsx";
+import ProductSale from "./product-sale.jsx";
+import {Type} from "../data.js";
+
+const getComponentByType = (type, product) => {
+  switch (type) {
+    case Type.SALE:
+      return <ProductSale product={product} />;
+    case Type.NEW:
+      return <ProductNew product={product} />;
+  }
+
+  return <Product product={product} />;
+};
+
+export default ({products}) => {
+  return (
+    <ul className="products">
+      {
+        products.map((product) => (
+          <li key={product.id}>
+            {getComponentByType(product.type, product)}
+          </li>
+        ))
+      }
+    </ul>
+  );
+};
